86df2646e5a9b187e7217f5040993273eeba9862
hafiyann <55193476+hafiyann@users.noreply.github.com>
2020-11-04 16:24:14 +0700

    Update userController.php

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -15,14 +15,10 @@ class userController extends Controller
 
     public function searchResult(){
 
-      // $nama_tempat = DB::select(`tempat.namaTempat`)
-      //                     		->from(`tempat`)
-      //                     		->join(`lapangan`, function($join) {
-      //                           			$join->on(`tempat.id`, `=`, `lapangan.id_tempat`);
-      //                           			})
-      //                     		->whereRaw(`lapangan.id NOT IN`, [], `( SELECT lapangan.id FROM orders join lapangan on orders.id_lapangan = lapangan.id join tempat on tempat.id = lapangan.id_tempat WHERE orders.tanggalPemesanan = "2020-11-04" and ( orders.start <= '14:00' and orders.end >= '16:00' ) or ( orders.start < '14:00' AND orders.end >= '16:00' ) or ( '14:00' <= orders.start AND '16:00' >= orders.end ) or ( orders.start BETWEEN '14:00' and '16:00' ) or ( orders.end BETWEEN '14:00' and '16:00' ))`)
-      //                     		->get();
+      // query sql
+      // SELECT tempat.namaTempat from tempat join lapangan on tempat.id=lapangan.id_tempat where lapangan.id NOT IN(SELECT lapangan.id FROM orders join lapangan on orders.id_lapangan=lapangan.id join tempat on tempat.id=lapangan.id_tempat WHERE orders.tanggalPemesanan = "2020-11-04" and (orders.start <= '14:00' and orders.end >= '16:00') or (orders.start < '14:00' AND orders.end >= '16:00') or ('14:00' <= orders.start AND '16:00' >= orders.end) or (orders.start BETWEEN '14:00' and '16:00') or (orders.end BETWEEN '14:00' and '16:00'))
 
+      //eloquent
       $results = tempat::join('lapangan', 'tempat.id', '=', 'lapangan.id_tempat')->select('tempat.id')->whereNotIn('lapangan.id', function($query){
         $query->select('lapangan.id')->from('orders')->join('lapangan','lapangan.id','=','orders.id_lapangan')->where('orders.tanggalPemesanan','2020-11-05');
         })->get();
